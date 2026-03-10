efbadd461a8bdc31cc52e19e53b1d7960002c8fe
Clément Charbonnel <c.charbo38@gmail.com>
2026-03-10 22:54:30 +0100

    feat(projet): ajout arborescence globale

---
+++ App_Logic/Inc/robot_config.h
@@ -0,0 +1,26 @@
+#ifndef MCU_ROBOT_PRINCIPAL_ROBOT_CONFIG_H
+#define MCU_ROBOT_PRINCIPAL_ROBOT_CONFIG_H
+
+/*-----------------------------------------
+ * Dimensions physique du robot
+ *-----------------------------------------*/
+
+#define WHEEL_RADIUS 0.05f  // Rayon des roues en mètres
+#define WHEEL_BASE 0.3f     // Distance entre les roues en mètres
+
+/*-----------------------------------------
+ * Paramètres de contrôle
+ *-----------------------------------------*/
+
+// Limites de vitesse
+#define MAX_LINEAR_VELOCITY 1.0f   // Vitesse linéaire maximale en m/s
+#define MAX_ANGULAR_VELOCITY 1.0f  // Vitesse angulaire maximale en rad/s
+
+// PID moteurs droit et gauche
+
+
+// PID moteur levage pince
+
+
+
+#endif  // MCU_ROBOT_PRINCIPAL_ROBOT_CONFIG_H
