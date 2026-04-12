9b67656f8e71b116fbe925209c1dd6770403d907
Clement Charbonnel <c.charbo38@gmail.com>
2026-04-12 18:57:05 +0200

    feat(servo_motor): ajoute l'initialisation et la gestion de l'angle pour un servo moteur

---
+++ Drivers_Custom/Inc/servo_motor.h
@@ -0,0 +1,31 @@
+#ifndef MCU_ROBOT_PRINCIPAL_SERVO_MOTOR_H
+#define MCU_ROBOT_PRINCIPAL_SERVO_MOTOR_H
+#include "stm32l4xx_hal.h"
+
+typedef struct {
+    TIM_HandleTypeDef* htim_pwm;
+    int channel_number;
+
+    float pulse_min;  // minimal pulse width in s
+    float pulse_max;
+
+    float angle_max;  // angle range of the servo (0 to ...)
+
+    uint8_t interrupt_mode;  // if 1, the timer will generate interrupts
+} ServoMotorConfig;
+
+typedef struct {
+    TIM_HandleTypeDef* htim_pwm;
+    int channel_number;
+
+    uint16_t ccr_min;
+    uint16_t ccr_max;
+
+    float angle_max;
+} ServoMotor;
+
+void init_servo_motor(ServoMotor* servo, ServoMotorConfig* cfg);
+
+int set_servo_angle(ServoMotor* servo, float angle);
+
+#endif  // MCU_ROBOT_PRINCIPAL_SERVO_MOTOR_H
---
+++ Drivers_Custom/Src/servo_motor.c
@@ -0,0 +1,35 @@
+#include "servo_motor.h"
+
+void init_servo_motor(ServoMotor* servo, ServoMotorConfig* cfg) {
+    servo->htim_pwm = cfg->htim_pwm;
+    servo->channel_number = cfg->channel_number;
+
+    servo->ccr_min =
+        (uint16_t)(cfg->pulse_min *
+                   (float)__HAL_TIM_GET_AUTORELOAD(servo->htim_pwm) / 0.02f);
+    servo->ccr_max =
+        (uint16_t)(cfg->pulse_max *
+                   (float)__HAL_TIM_GET_AUTORELOAD(servo->htim_pwm) / 0.02f);
+
+    servo->angle_max = cfg->angle_max;
+
+    set_servo_angle(servo, 0.0f);
+
+    if (cfg->interrupt_mode == 1)
+        HAL_TIM_PWM_Start_IT(cfg->htim_pwm, cfg->channel_number);
+    else
+        HAL_TIM_PWM_Start(cfg->htim_pwm, cfg->channel_number);
+}
+
+int set_servo_angle(ServoMotor* servo, float angle) {
+    if (angle > servo->angle_max) return -1;
+    if (angle < 0) return -1;
+
+    __HAL_TIM_SET_COMPARE(
+        servo->htim_pwm, servo->channel_number,
+        servo->ccr_min +
+            (uint16_t)(angle * (float)(servo->ccr_max - servo->ccr_min) /
+                       servo->angle_max));
+
+    return 0;
+}
